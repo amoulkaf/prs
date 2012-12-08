d3a79e13c113e95bc34c9f72895d94aa98decb1b
Alexandre Chapoulie <achapoul@emi.u-bordeaux1.fr>
2012-12-08 17:34:58 +0100

    Ajout du fichier de desarchivage : desarchivage.c

---
+++ desarchivage.c
@@ -0,0 +1,87 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <time.h>
+#include <string.h>
+#include <dirent.h>
+#include "outils.h"
+#include <math.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+
+struct dateModif{
+  int day;
+  int month;
+  int year;
+  int hour;
+  int minute;
+  int second;
+};
+
+
+#define True 1
+#define False 0
+
+ void creationFichierArchive(char * name, char * file, int size)
+{
+  int fd = open(name,O_WRONLY|O_CREAT,0666);
+  
+    write(fd,file,size);
+  
+  close(fd);
+  
+}
+
+bool ifDossier(char * name)
+{
+  int i;
+  for(i=0;i=sizeof(name);i++)
+    if(name[i]=='/')
+      return True;
+  return False;
+}
+
+void creerDossierArchive(char * name)
+{
+  mkdir(name,0766);
+}
+ 
+int main(int argc, char **argv)
+{
+
+  char * archive = argv[1];
+  int fd = open(archive,O_RDONLY);
+  int size;
+  char * file;
+  char * name;
+  dateModif date;
+  char c;
+
+
+  while (read(fd,&c,1 >0)){ //test lecture possible
+
+    name = getNextFileName(fd);
+    size = getNextFileSize(fd);
+    date =lastModifedArchive(fd);
+    file = readNextFile(fd,size);
+
+    if(ifDossier (name)){
+
+      creerDossierArchive(name);
+    
+    }else{
+ 
+      creationFichierArchive(name,file,size);
+    
+    }
+
+  }
+ close(fd);
+
+}
+
+ 
